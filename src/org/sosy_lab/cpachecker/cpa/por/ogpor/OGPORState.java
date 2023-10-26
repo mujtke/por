29463c0206d780b116b6992d7d7207c727f922a2
mujtke <mujueke@outlook.com>
2023-10-26 11:46:37 +0800

    Rewrite the loop depth method, fix an error about nested locks

--- src/org/sosy_lab/cpachecker/cpa/por/ogpor/OGPORState.java
+++ src/org/sosy_lab/cpachecker/cpa/por/ogpor/OGPORState.java
@@ -4,6 +4,7 @@ package org.sosy_lab.cpachecker.cpa.por.ogpor;
 import com.google.common.base.Preconditions;
 import org.junit.Assume;
 import org.sosy_lab.common.annotations.ReturnValuesAreNonnullByDefault;
+import org.sosy_lab.cpachecker.cfa.CFA;
 import org.sosy_lab.cpachecker.cfa.model.AssumeEdge;
 import org.sosy_lab.cpachecker.cfa.model.BlankEdge;
 import org.sosy_lab.cpachecker.cfa.model.CFAEdge;
@@ -11,16 +12,19 @@ import org.sosy_lab.cpachecker.cfa.model.CFANode;
 import org.sosy_lab.cpachecker.cfa.model.c.CDeclarationEdge;
 import org.sosy_lab.cpachecker.core.interfaces.AbstractState;
 import org.sosy_lab.cpachecker.core.interfaces.Graphable;
+import org.sosy_lab.cpachecker.util.LoopStructure;
 import org.sosy_lab.cpachecker.util.Pair;
-import scala.Int;
+import org.sosy_lab.cpachecker.util.LoopStructure.Loop;
 
 import javax.annotation.Nonnull;
 import java.util.*;
+import java.util.stream.Collectors;
 
 import static java.util.Objects.hash;
 
 public class OGPORState implements AbstractState, Graphable {
 
+    private static CFA cfa;
     private int num;
     // Assume there is an edge: sn -- Ei --> sm, then the value of 'inThread' will be
     // the activeThread of 'Ei', which comes from the threadingState in sn. We set
@@ -38,8 +42,7 @@ public class OGPORState implements AbstractState, Graphable {
     private final Map<String, Stack<CFANode>> loops = new HashMap<>();
     // The variable is used to record the depth of each loop we have met.
     private final Map<CFANode, Integer> loopDepthTable = new HashMap<>();
-    private final Map<CFANode, CFANode> loopExitNodes = new HashMap<>();
-    private final static Map<CFANode, Pair<Integer, Integer>> loopRange = new HashMap<>();
+    private static final Map<CFANode, Set<CFANode>> loopExitNodes = new HashMap<>();
 
     @Override
     public int hashCode() {
@@ -81,6 +84,26 @@ public class OGPORState implements AbstractState, Graphable {
     public OGPORState(int pNum) {
         num = pNum;
         threads = new HashMap<>();
+    }
+
+    public void setLoopInfo() {
+        Preconditions.checkArgument(cfa.getLoopStructure().isPresent(),
+                "Missing loop structure.");
+        LoopStructure loopStructure = cfa.getLoopStructure().get();
+        for (Loop loop : loopStructure.getAllLoops()) {
+            // one loop just have one loop head?
+            Set<CFANode> loopStarts = loop.getIncomingEdges()
+                    .stream().map(CFAEdge::getSuccessor).collect(Collectors.toSet());
+            Preconditions.checkState(loopStarts.size() == 1,
+                    "Just one loop start node is expected.");
+            CFANode loopStart= loopStarts.iterator().next();
+            Preconditions.checkState(loopStart.isLoopStart(),
+                    "Setting error node as loop start.");
+            // Corresponding loopStart to its loop exit nodes.
+            Set<CFANode> loopExits = loop.getOutgoingEdges()
+                    .stream().map(CFAEdge::getSuccessor).collect(Collectors.toSet());
+            loopExitNodes.put(loopStart, loopExits);
+        }
     }
 
     public Map<String, String> getThreads() {
@@ -131,6 +154,10 @@ public class OGPORState implements AbstractState, Graphable {
         this.num = pNum;
     }
 
+    public void setCfa(CFA pCfa) {
+        cfa = pCfa;
+    }
+
     public void updateLoopDepth(CFAEdge cfaEdge) {
         Preconditions.checkArgument(cfaEdge != null,
                 "A CFA edge is required.");
@@ -144,32 +171,6 @@ public class OGPORState implements AbstractState, Graphable {
         }
 
         if (pre.isLoopStart()) {
-            // If we haven't computed the range for the pre.
-            if (!loopRange.containsKey(pre)) {
-                Preconditions.checkState(pre.getNumEnteringEdges() == 2, "Just two " +
-                        "edges entering to a loop start node is expected.");
-                CFAEdge loopStartEdge = pre.getEnteringEdge(0),
-                        loopBackEdge = pre.getEnteringEdge(1);
-                // Get the start line of the loop.
-                int startLine = loopStartEdge.getFileLocation().getStartingLineNumber(),
-                        endLine = -1;
-                // Get the end line of the loop.
-                CFANode lbePre = loopBackEdge.getPredecessor();
-                if (!(loopBackEdge instanceof BlankEdge)) {
-                    Preconditions.checkArgument(lbePre.getNumEnteringEdges() == 1);
-                    loopBackEdge = lbePre.getEnteringEdge(0);
-                    lbePre = loopBackEdge.getPredecessor();
-                }
-                for (int i = 0; i < lbePre.getNumEnteringEdges(); i++) {
-                    CFAEdge ei = lbePre.getEnteringEdge(i);
-                    if (ei instanceof BlankEdge) continue;
-                    int tempLine = ei.getFileLocation().getEndingLineNumber();
-                    if (tempLine > endLine) endLine = tempLine;
-                }
-                Preconditions.checkState(endLine > 0,
-                        "Finding loop end line failed.");
-                loopRange.put(pre, Pair.of(startLine, endLine));
-            }
             if (!pre.equals(curLoop)) {
                 // curLoop is null or pre != curLoop, which means we reach a new loop
                 // start. We add a new loop item with initial depth = 1.
@@ -183,24 +184,12 @@ public class OGPORState implements AbstractState, Graphable {
             }
         } else {
             // Pre may be a loop exit node.
-            if (curLoop != null && pre.equals(loopExitNodes.get(curLoop))) {
-                // If pre is a loop exit node, then we exit the curLoop.
-                loopDepthTable.remove(curLoop);
-                loops.get(inThread).pop();
-            } else if (curLoop != null && !loopExitNodes.containsKey(curLoop)) {
-                // We haven't computed the exited node for the curLoop.
-                if (!(cfaEdge instanceof BlankEdge)) {
-                    // BlankEdge may have no file location.
-                    Preconditions.checkArgument(loopRange.containsKey(curLoop));
-                    int curLine = cfaEdge.getFileLocation().getStartingLineNumber(),
-                            curLoopEndLine = loopRange.get(curLoop).getSecondNotNull();
-                    if (curLine > curLoopEndLine) {
-                        // curLine has exit the curLoop, and we add the exited node for
-                        // curLoop in loopExitNodes.
-                        loopDepthTable.remove(curLoop);
-                        loops.get(inThread).pop();
-                        loopExitNodes.put(curLoop, pre);
-                    }
+            if (curLoop != null) {
+                Preconditions.checkArgument(loopExitNodes.containsKey(curLoop));
+                if (loopExitNodes.get(curLoop).contains(pre)) {
+                    // If pre is a loop exit node, then we exit the curLoop.
+                    loopDepthTable.remove(curLoop);
+                    loops.get(inThread).pop();
                 }
             }
         }
