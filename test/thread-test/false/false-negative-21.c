4afc0407f4c8c678f8137be41a46229007545c65
mujtke <mujueke@outlook.com>
2024-05-06 17:56:45 +0800

    Revert "Storing."
    This reverts commit d077dea16534058c7cbf735f2e08e9f370b5aa82.

--- test/thread-test/false/false-negative-21.c
+++ test/thread-test/false/false-negative-21.c
@@ -158,7 +158,7 @@ void * P0(void *arg)
   __VERIFIER_atomic_begin();
 //   z = w_buff0_used && r_buff0_thd1 ? w_buff0 : (w_buff1_used && r_buff1_thd1 ? w_buff1 : z);
 //   w_buff0_used = w_buff0_used && r_buff0_thd1 ? FALSE : w_buff0_used;
-//   w_buff0_used = w_buff0_used;
+  w_buff0_used = w_buff0_used;
 //   w_buff1_used = w_buff0_used && r_buff0_thd1 || w_buff1_used && r_buff1_thd1 ? FALSE : w_buff1_used;
   w_buff1_used = w_buff1_used;
 //   r_buff0_thd1 = w_buff0_used && r_buff0_thd1 ? FALSE : r_buff0_thd1;
@@ -229,11 +229,11 @@ int main()
   __VERIFIER_atomic_begin();
 //   z = w_buff0_used && r_buff0_thd0 ? w_buff0 : (w_buff1_used && r_buff1_thd0 ? w_buff1 : z);
 //   w_buff0_used = w_buff0_used && r_buff0_thd0 ? FALSE : w_buff0_used;
-//   w_buff0_used = w_buff0_used ? FALSE : w_buff0_used;
+  w_buff0_used = w_buff0_used ? FALSE : w_buff0_used;
 //   w_buff1_used = w_buff0_used && r_buff0_thd0 || w_buff1_used && r_buff1_thd0 ? FALSE : w_buff1_used;
   w_buff1_used = w_buff1_used;
 //   r_buff0_thd0 = w_buff0_used && r_buff0_thd0 ? FALSE : r_buff0_thd0;
-  r_buff1_thd0 = w_buff0_used && r_buff0_thd0 || w_buff1_used && r_buff1_thd0 ? FALSE : r_buff1_thd0;
+//   r_buff1_thd0 = w_buff0_used && r_buff0_thd0 || w_buff1_used && r_buff1_thd0 ? FALSE : r_buff1_thd0;
   __VERIFIER_atomic_end();
 
   __VERIFIER_atomic_begin();
@@ -246,3 +246,4 @@ int main()
 
   return 0;
 }
+
