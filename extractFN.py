f8394953ea2bb164463016d93e0c949fa253d7ee
mujtke <mujueke@outlook.com>
2024-06-28 12:13:10 +0800

    Fix relative path for FN.set.

--- extractFN.py
+++ extractFN.py
@@ -26,16 +26,17 @@ if not os.path.exists(dataJson):
 if (os.system(f'grep \'"rows":\' {dataDir}/*.html | sed \'s/^ *"rows": *\\(.*\\),$/\\1/\' > {dataJson}') != 0):
     raise FileExistsError(f"extracting data from .html file failed!")
 
-if not os.path.exists(dataDir + "/FN.set"):
-    os.system("touch " + dataDir + "/FN.set")
-fnSet = open(dataDir + "/FN.set", 'w')
+fnSetFile = "./FN.set"
+if not os.path.exists(fnSetFile):
+    os.system("touch " + fnSetFile)
+fnSet = open(fnSetFile, 'w')
 
 data = json.load(open(dataJson))
 for row in data:
     wrong = filter(lambda x : x['category'] == 'wrong', row['results'])
     if (list(wrong)):
         print(re.sub('../', '', row["href"], 1))
-        fnSet.write('../' + row["href"] + "\n")
+        fnSet.write(re.sub('../', '', row["href"], 1) + "\n")
 
-setFile = (dataDir + "/FN.set").replace('/', '\/')
-os.system(f'sed -i\'.bak\' \'s/\(<includesfile>\).*\(<\/includesfile>\)/\\1{setFile}\\2/\' OGPOR-FN.xml')
+fnSetFile = fnSetFile.replace('/', '\/')
+os.system(f'sed -i\'.bak\' \'s/\(<includesfile>\).*\(<\/includesfile>\)/\\1{fnSetFile}\\2/\' OGPOR-FN.xml')
