00b746825c75a25107f9cfb49a02ee0ecd21be90
mujtke <mujueke@outlook.com>
2024-10-31 15:19:29 +0800

    Fix an error in OGRevistor#maximallyAdded.

--- src/org/sosy_lab/cpachecker/core/algorithm/og/OGRevisitor.java
+++ src/org/sosy_lab/cpachecker/core/algorithm/og/OGRevisitor.java
@@ -357,8 +357,9 @@ public class OGRevisitor {
 
     for (SharedEvent epmo : ep.getAllMoBefore()) {
       // FIXME: if epmo locates in w.inNode or r.inNode?
-      boolean cond = previous.contains(epmo)
-              && !Objects.equals(epmo.getInNode(), w.getInNode());
+//      boolean cond = previous.contains(epmo)
+//              && !Objects.equals(epmo.getInNode(), w.getInNode());
+      boolean cond = previous.contains(epmo);
       if (cond) {
         // ep \in previous /\ \exists epmo \in previous s.t. <ep, epmo>
         // \in G.mo /\ ep, epmo not in the same block (FIXME: with w or r?)
